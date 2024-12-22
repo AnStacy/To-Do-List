bedd60703adea4137c67877d45a1bf7ececc579b
AnStacy <niaiko.ana.stasia@gmail.com>
2024-12-22 14:34:01 -0600

    Fixed slyle for sidebar

--- js/burger-menu.js
+++
@@ -1,6 +0,0 @@
-const burgerMenu = document.querySelector(".burger-menu");
-const sidebar = document.querySelector(".sidebar");
-
-burgerMenu.addEventListener("click", () => {
-  sidebar.classList.toggle("active");
-});
